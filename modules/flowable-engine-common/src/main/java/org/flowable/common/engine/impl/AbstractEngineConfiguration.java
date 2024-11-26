4e6939ea4f4333b15e3eb9597d6df665e106df30
WANG.Qing <2362072650@qq.com>
2024-11-26 09:41:28 +0800

    添加虚谷产品名称XuguDB

--- modules/flowable-engine-common/src/main/java/org/flowable/common/engine/impl/AbstractEngineConfiguration.java
+++ modules/flowable-engine-common/src/main/java/org/flowable/common/engine/impl/AbstractEngineConfiguration.java
@@ -410,6 +410,7 @@ public abstract class AbstractEngineConfiguration {
         databaseTypeMappings.setProperty("DB2 UDB AS400", DATABASE_TYPE_DB2);
         databaseTypeMappings.setProperty(PRODUCT_NAME_CRDB, DATABASE_TYPE_COCKROACHDB);
         databaseTypeMappings.setProperty("XuGU", DATABASE_TYPE_XUGU);
+        databaseTypeMappings.setProperty("XuguDB", DATABASE_TYPE_XUGU);
         return databaseTypeMappings;
     }
 
