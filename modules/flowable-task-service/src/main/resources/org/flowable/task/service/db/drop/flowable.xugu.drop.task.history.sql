5f259e68fac9e235a15bc27269de924f0981e8e2
WANG Qing <2362072650@qq.com>
2025-01-16 17:45:22 +0800

    sql脚本适配

--- modules/flowable-task-service/src/main/resources/org/flowable/task/service/db/drop/flowable.xugu.drop.task.history.sql
+++ modules/flowable-task-service/src/main/resources/org/flowable/task/service/db/drop/flowable.xugu.drop.task.history.sql
@@ -2,5 +2,5 @@ drop index ACT_HI_TASKINST.ACT_IDX_HI_TASK_SCOPE;
 drop index ACT_HI_TASKINST.ACT_IDX_HI_TASK_SUB_SCOPE;
 drop index ACT_HI_TASKINST.ACT_IDX_HI_TASK_SCOPE_DEF;
 
-drop table if exists ACT_HI_TASKINST;
-drop table if exists ACT_HI_TSK_LOG;
+drop table if exists ACT_HI_TASKINST cascade;
+drop table if exists ACT_HI_TSK_LOG cascade;
